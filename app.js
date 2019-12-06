2d1e8b8040869bc467872d1887cdaf591111cd52
Bruno Borghese <bb_borghese@hotmail.com>
2019-12-06 00:24:41 -0200

    arquivos de rotas

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
