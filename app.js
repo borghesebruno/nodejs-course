2a33d408dfee61c23bb50c25dbe7ae0dd5628726
Bruno Borghese <bb_borghese@hotmail.com>
2020-06-29 00:09:06 -0300

    removing unused things

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { use } = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
