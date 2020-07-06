69c66e9e8ffbc86048a35373265150fa06c39b1d
Bruno Borghese <bb_borghese@hotmail.com>
2020-07-05 23:47:51 -0300

    complete use of mongoose, and .prettierrc

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById("5ef95252ccf93735e065d819")
-//         .then((user) => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('5f028d9d8c8b5d1e98b93c3c')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoose
     .connect(
-        "mongodb+srv://nodejscourse:nodejsmongo@cluster0-no6hz.mongodb.net/shop?retryWrites=true&w=majority"
+        'mongodb+srv://nodejscourse:nodejsmongo@cluster0-no6hz.mongodb.net/shop?retryWrites=true&w=majority'
     )
-    .then(() => {
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Bruno',
+                    email: 'b@b.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
         app.listen(3000);
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,10 @@
-const Product = require("../models/product");
-const product = require("../models/product");
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render("admin/edit-product", {
-        pageTitle: "Add Product",
-        path: "/admin/add-product",
-        editing: false,
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
     });
 };
 
@@ -19,14 +18,15 @@ exports.postAddProduct = (req, res, next) => {
         price,
         description,
         imageUrl,
+        userId: req.user
     });
     product
         .save()
-        .then((result) => {
-            console.log("Created Product");
-            res.redirect("/admin/products");
+        .then(result => {
+            console.log('Created Product');
+            res.redirect('/admin/products');
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         });
 };
@@ -34,22 +34,22 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
     if (!editMode) {
-        res.redirect("/");
+        res.redirect('/');
     }
     const prodId = req.params.productId;
     Product.findById(prodId)
-        .then((product) => {
+        .then(product => {
             if (!product) {
-                res.redirect("/");
+                res.redirect('/');
             }
-            res.render("admin/edit-product", {
-                pageTitle: "Edit Product",
-                path: "/admin/add-product",
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/add-product',
                 product: product,
-                editing: editMode,
+                editing: editMode
             });
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         });
 };
@@ -61,38 +61,40 @@ exports.postEditProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
     Product.findById(prodId)
-        .then((product) => {
+        .then(product => {
             product.title = title;
             product.imageUrl = imageUrl;
             product.price = price;
             product.description = description;
             product.save();
         })
-        .then((result) => {
-            console.log("UPDATED PRODUCT!");
-            res.redirect("/admin/products");
+        .then(result => {
+            console.log('UPDATED PRODUCT!');
+            res.redirect('/admin/products');
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
     Product.find()
-        .then((products) => {
-            res.render("admin/products", {
+        // .select('title price -_id')
+        // .populate('userId', 'name')
+        .then(products => {
+            res.render('admin/products', {
                 prods: products,
-                pageTitle: "Admin Products",
-                path: "/admin/products",
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
             });
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findByIdAndRemove(prodId)
         .then(() => {
-            console.log("DESTROYED PRODUCT");
-            res.redirect("/admin/products");
+            console.log('DESTROYED PRODUCT');
+            res.redirect('/admin/products');
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
 };
